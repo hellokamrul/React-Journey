41ca1927f4ba7e7bde57f32bf170da6665b1f2e7
unknown <nijumkamrul447@gmail.com>
2025-01-26 00:42:54 +0600

    add project section

--- kamrul-portfolio/src/components/Projects.jsx
+++ kamrul-portfolio/src/components/Projects.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React from "react";
+import erp from "../assets/erp.jpg";
+import Accounting from "../assets/Accounting.jpg";
+import Inventory from "../assets/inventory.jpg";
 
 const Projects = () => {
+  const projects = [
+    {
+      id:1,
+      src: erp,
+    },
+    {
+      id:2,
+      src: Accounting,
+    },
+    {
+      id:3,
+      src: Inventory,
+    },
+    {
+      id:4,
+      src: Inventory,
+    },
+  ];
   return (
-    <div>Projects</div>
-  )
-}
+    <div name="projects" className="pl-15 w-full bg-gradient-to-b from-black to-gray-800 text-white">
+      <div className="max-w-screen-lg p-4 max-auto flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">Projects</p>
+          <p className="py-6">Check out some of my work right here</p>
+        </div>
+        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
+          {projects.map(({ id, src }) => (
+            <div key={id} className="shadow-md shadow-gray-600 rounded-lg ">
+              <img src={src} alt={`project-${id}`} className="rounded-md duration-200 hover:scale-105" />
+              <div className="flex items-center justify-center">
+                <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">Demo</button>
+                <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">Code</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Projects
+export default Projects;
